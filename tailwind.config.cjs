97b2a83561b8bf53bbb12d8332b76ecab9bfa4df
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-04 22:25:08 +0530

    theme color added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		colors: {
+			white: '#fff',
+			colorBg: '#040C18',
+			colorBlog: '#042c54',
+			colorText: '#81AFDD',
+			colorSubtext: '#FF8A71',
+			colorButton: '#ff4820',
+		},
 		extend: {},
 	},
 	plugins: [],
