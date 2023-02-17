97b53828d6962b5b405bd971a2ef8a7c6bdf8aac
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-17 23:04:50 +0530

    header padding fixed and brand section added and styled

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import googleBrandImg from '../assets/google.png';
+import slackBrandImg from '../assets/slack.png';
+import altassianBrandImg from '../assets/atlassian.png';
+import dropboxBrandImg from '../assets/dropbox.png';
+import shopifyBrandImg from '../assets/shopify.png';
 
 const Brand = () => {
-	return <div>Brand</div>;
+	return (
+		<div className="flex flex-wrap items-center justify-center gap-10 py-16 xl:px-24 ">
+			<img src={googleBrandImg} alt="googleBrandImg" />
+			<img src={slackBrandImg} alt="slackBrandImg" />
+			<img src={altassianBrandImg} alt="altassianBrandImg" />
+			<img src={dropboxBrandImg} alt="dropboxBrandImg" />
+			<img src={shopifyBrandImg} alt="shopifyBrandImg" />
+		</div>
+	);
 };
 
 export default Brand;
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,42 +1,45 @@
 import people from '../assets/people.png';
 import ai from '../assets/ai.png';
-import { Button } from '../components';
+import { Brand, Button } from '../components';
 
 const Header = () => {
 	return (
-		<div className="flex flex-col items-center gap-10 lg:gap-0 px-10 py-16 lg:flex-row lg:pl-16 xl:pl-24">
-			<div className="flex flex-col gap-8 align-start md:w-9/12 lg:w-full basis-1/2">
-				<h1 className="text-gradient text-5xl xl:text-6xl font-semibold">
-					Let’s Build Something amazing with GPT-3 OpenAI
-				</h1>
-				<p className=" md:w-9/12">
-					Yet bed any for travelling assistance indulgence unpleasing. Not
-					thoughts all exercise blessing. Indulgence way everything joy
-					alteration boisterous the attachment. Party we years to order allow
-					asked of.
-				</p>
+		<>
+			<div className="flex flex-col items-center gap-10 lg:gap-0 px-10 py-16 lg:py-0 lg:flex-row lg:pl-16 xl:pl-24">
+				<div className="flex flex-col gap-8 align-start md:w-9/12 lg:w-full basis-1/2">
+					<h1 className="text-gradient text-5xl xl:text-6xl font-semibold">
+						Let’s Build Something amazing with GPT-3 OpenAI
+					</h1>
+					<p className=" md:w-9/12">
+						Yet bed any for travelling assistance indulgence unpleasing. Not
+						thoughts all exercise blessing. Indulgence way everything joy
+						alteration boisterous the attachment. Party we years to order allow
+						asked of.
+					</p>
 
-				<div className="bg-colorInput md:w-9/12 flex rounded-md">
-					<input
-						type="email"
-						placeholder="Your Email Address"
-						className="w-3/5 p-4 bg-colorInput rounded-md"
-					/>
-					<Button
-						className="w-2/5 bg-colorButton text-white font-medium text-lg rounded-r-lg"
-						name="Get Started"
-					/>
+					<div className="bg-colorInput md:w-9/12 flex rounded-md">
+						<input
+							type="email"
+							placeholder="Your Email Address"
+							className="w-3/5 p-4 bg-colorInput rounded-md"
+						/>
+						<Button
+							className="w-2/5 bg-colorButton text-white font-medium text-lg rounded-r-lg"
+							name="Get Started"
+						/>
+					</div>
+
+					<div className="flex flex-col items-start md:flex-row md:items-center gap-4 font-medium text-white">
+						<img src={people} alt="people" />
+						<p>1,600 people requested access a visit in last 24 hours</p>
+					</div>
 				</div>
-
-				<div className="flex flex-col items-start md:flex-row md:items-center gap-4 font-medium text-white">
-					<img src={people} alt="people" />
-					<p>1,600 people requested access a visit in last 24 hours</p>
+				<div className="lg:basis-1/2">
+					<img src={ai} alt="ai" />
 				</div>
 			</div>
-			<div className="lg:basis-1/2">
-				<img src={ai} alt="ai" />
-			</div>
-		</div>
+			<Brand />
+		</>
 	);
 };
 
