d3c19803b2d5dc76137f2d89fe71f281d03aa90b
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-18 23:06:49 +0530

    blog imported

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Article from './Article';
+import Blog from './Blog';
 import Brand from './Brand';
 import CTA from './CTA';
 import Feature from './Feature';
 import Navbar from './Navbar';
+import Button from './Button';
 
-export { Article, Brand, CTA, Feature, Navbar };
+export { Article, Blog, Brand, CTA, Feature, Navbar, Button };
