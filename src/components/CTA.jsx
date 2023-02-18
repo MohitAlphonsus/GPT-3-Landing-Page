791e4fd49e1e5c4e30c8bc3fb94d0adb780e8cda
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-02-18 23:08:07 +0530

    CTA section added

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Button from './Button';
+
+const buttonClasses =
+	'font-semibold text-colorSubtext border-b-2 border-transparent pb-2 transition-all  hover:border-currentColor';
 
 const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+	return (
+		<div className="px-12 lg:px-16 xl:px-24 py-20">
+			<div className="gradient-bar text-[#000] flex flex-col lg:flex-row items-start lg:items-center gap-8 lg:justify-between p-10 rounded-md">
+				<div>
+					<Button
+						className={`${buttonClasses} text-[#000]`}
+						name="Request Early Access to Get Started"
+					/>
+					<p className="text-2xl font-semibold">
+						Register today & start exploring the endless possiblities.
+					</p>
+				</div>
+				<Button
+					className="bg-colorBg text-white py-4 px-10 rounded-full hover:bg-[#000]"
+					name="Get Started"
+				/>
+			</div>
+		</div>
+	);
+};
 
-export default CTA
+export default CTA;
